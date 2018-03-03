46007fc8d99473a0c96a192072cd6f66457bcc0f
Lindsay M <lminami23@punahou.edu>
2018-03-03 10:29:55 -1000

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -12,31 +12,70 @@ window.onload = function(){
 
 		return false;
 	}
+
 	function treehousesRCool(){
+		toDoCreate()
+		postItCreate()
+	}
+
+	function toDoCreate(){
 		var list = document.getElementById('list');
 		var add = document.getElementById('addToDo');
-		var noteContainer = document.getElementById('note-container');
-
 		add.addEventListener('click', function(){
 			var newInput = document.createElement('input');
 			newInput.setAttribute('class', 'thing-toDo');
 			var newDiv = document.createElement('div');
 			newDiv.setAttribute('class', 'toDo-div');
-			newDiv.appendChild(newInput);
-			list.appendChild(newDiv)
-			newInput.addEventListener('keydown', function(e){
-				//console.log(e.keyCode)
+			var anotherNewDiv = document.createElement('div');
+			anotherNewDiv.setAttribute('class', 'another-toDo-div');
+			var xButton = document.createElement('button');
+			xButton.setAttribute('class', 'xButton');
+
+			anotherNewDiv.appendChild(newInput);
+			newDiv.appendChild(xButton);
+			newDiv.appendChild(anotherNewDiv)
+			list.appendChild(newDiv);
+
+			document.addEventListener('keydown', function(e){
 				if (e.keyCode == 13) {
-					console.log('Enter! >_<')
-					//console.log(newInput.value)
-					newDiv.innerHTML = newInput.value;
+					console.log('Enter! >_<');
 					newInput.style.display = 'none';
+					anotherNewDiv.innerHTML = newInput.value;	
 				}
 			})
-			newDiv.addEventListener('click', function(){
+
+			xButton.addEventListener('click', function(){
 				console.log('I was clicked!')
 				newDiv.style.display = 'none'
-			}, true)
-		})	
+			})
+		}, false)	
+	}
+
+	function postItCreate(){		
+		var noteContainer = document.getElementById('note-container');
+		var cords = document.createElement('p');
+		noteContainer.appendChild(cords)
+		let x;
+		let y;
+		noteContainer.addEventListener('mousemove', function(){
+			x = event.clientX;
+			y = event.clientY;
+			cords.innerHTML = 'x: ' + x + ' y: ' + y;
+		});
+		
+		noteContainer.addEventListener('click', function(){
+			var sticky = document.createElement('div');
+			sticky.setAttribute('class', 'sticky location');
+			var stickInput = document.createElement('input');
+			stickInput.setAttribute('class', 'stickyInput location');
+
+			sticky.style.left = x + 'px';
+			stickInput.style.left = x + 'px';
+			sticky.style.top = y + 'px';
+			stickInput.style.top = y + 'px';
+
+			sticky.appendChild(stickInput);
+			noteContainer.appendChild(sticky)
+		}, false)
 	}
 }
